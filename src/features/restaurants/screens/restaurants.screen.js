f14b0578fe3889185550908490293f05adb3fb57
skarki1200 <karkisunil1200@gmail.com>
2022-06-25 06:33:53 -0700

    Fixed android height issue

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,13 +1,12 @@
 import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
 import { Searchbar } from "react-native-paper";
 import React from "react";
-import RestaurntInfoCard from "../components/restaurant-info-card";
+import RestaurantInfoCard from "../components/restaurant-info-card";
 import styled from "styled-components/native";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  // this fixes the height problem on IOS
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
@@ -17,20 +16,17 @@ const SearchContainer = styled.View`
 const RestaurantListContainer = styled.View`
   flex: 1;
   padding: ${(props) => props.theme.space[3]};
-  background-color: #bbecf7;
 `;
 
-const RestaurantsScreen = () => {
-  return (
-    <SafeArea>
-      <SearchContainer>
-        <Searchbar placeholder="search..." />
-      </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurntInfoCard />
-      </RestaurantListContainer>
-    </SafeArea>
-  );
-};
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
+);
 
 export default RestaurantsScreen;
